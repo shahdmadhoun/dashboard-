b1e8be136f0be3bb166fd03640da6b6616110778
shahdmadhoun <shahdem100@gmail.com>
2022-03-14 02:22:32 -0700

    change theme

--- main.js
+++ main.js
@@ -24,3 +24,26 @@ new Chart(chart, {
         responsive: true
     }
 })
+
+// show or hide side-bar
+const menuBtn = document.querySelector('#menu-btn');
+const closeBtn = document.querySelector('#close-btn');
+const sideBar = document.querySelector('aside');
+
+menuBtn.addEventListener('click', () => {
+    sideBar.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', () => {
+    sideBar.style.display = 'none';
+})
+
+// change theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+
+    themeBtn.querySelector('span:first-child').classList.toggle('active');
+
+    themeBtn.querySelector('span:last-child').classList.toggle('active');
+})
